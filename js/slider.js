9cf142a9e679d80663f072be004264450c0a64ef
tsedor <itsme199@gmail.com>
2020-05-05 17:44:48 +0200

    touch events for swipe on mobile devices

--- js/slider.js
+++ js/slider.js
@@ -6,10 +6,11 @@ class Slider {
     this.activeSlideId = 1;
     this.slides = document.querySelectorAll('.slide');
     this.dots = document.querySelectorAll('.slider__dots--item');
-    setInterval(() => this.nextSlide(), 5000);
+    this.swipe();
+    this.sliderInterval = setInterval(() => this.nextSlide(), 5000);
   }
   nextSlide(id = 0) {
-    const nextSlideId = this.activeSlideId === this.slides.length ? 1 : this.activeSlideId + 1;
+    const nextSlideId = id === 0 ? this.activeSlideId === this.slides.length ? 1 : this.activeSlideId + 1 : id;
     
     this.setCurrentDot(nextSlideId-1);
     this.nextSlideAnimation(nextSlideId);
@@ -22,6 +23,43 @@ class Slider {
     tl.set(`.slide:nth-child(${nextSlideId+1})`, { zIndex: 10, display: 'block' });
     tl.from(`.slide:nth-child(${nextSlideId+1})`, { x: '100%', duration: .5 });
     tl.set(`.slide:nth-child(${this.activeSlideId+1})`, { display: 'none' });
+    this.textAnimation(nextSlideId, tl);
+  }
+  swipe() {
+    let cords = {
+      start: {
+        clientX: 0,
+        clientY: 0
+      },
+      end: {
+        clientX: 0,
+        clientY: 0
+      }
+    }
+    const slider = document.querySelector('.slider');
+    slider.addEventListener('touchstart', e => {
+      const { clientX, clientY } = e.touches[0];
+      cords.start = {
+        clientX,
+        clientY
+      }
+    });
+    slider.addEventListener('touchmove', e => {
+      const { clientX, clientY } = e.touches[0];
+      cords.end = {
+        clientX,
+        clientY
+      }
+    })
+    slider.addEventListener('touchend', e => {
+      clearInterval(this.sliderInterval);
+      cords.start.clientX > cords.end.clientX ? this.nextSlide() : this.nextSlide(this.activeSlideId - 1 === 0 ? this.slides.length : this.activeSlideId - 1);
+      this.sliderInterval = setInterval(() => this.nextSlide(), 5000);
+    });
+  }
+  textAnimation(nextSlideId, tl) {
+    tl.from(`.slide:nth-child(${nextSlideId+1}) p`, { y: '100%', opacity: .2, duration: .3, stagger: .2 });
+    tl.from(`.slide:nth-child(${nextSlideId+1}) .slide__subtitle`, { y: 30, opacity: 0, duration: .3 }, 1);
   }
   setCurrentDot(nextSlideId) {
     Array.from(this.dots).map(dot => {
