36b18374b2ad5e1c5669e63e9887cd83d63df54d
tsedor <itsme199@gmail.com>
2020-05-05 19:49:16 +0200

    better UX for slider

--- js/slider.js
+++ js/slider.js
@@ -4,10 +4,18 @@ class Slider {
   constructor() {
     this.interval = 5000;
     this.activeSlideId = 1;
+    this.previous = false;
     this.slides = document.querySelectorAll('.slide');
     this.dots = document.querySelectorAll('.slider__dots--item');
     this.swipe();
     this.sliderInterval = setInterval(() => this.nextSlide(), 5000);
+
+    Array.from(this.dots).map((dot, i) => {
+      dot.addEventListener('click', () => {
+        i+1 !== this.activeSlideId && this.nextSlide(i+1);
+        this.sliderIntervalReset();
+      })
+    })
   }
   nextSlide(id = 0) {
     const nextSlideId = id === 0 ? this.activeSlideId === this.slides.length ? 1 : this.activeSlideId + 1 : id;
@@ -21,9 +29,14 @@ class Slider {
     
     tl.set(`.slide:nth-child(${this.activeSlideId+1})`, { zIndex: 9 });
     tl.set(`.slide:nth-child(${nextSlideId+1})`, { zIndex: 10, display: 'block' });
-    tl.from(`.slide:nth-child(${nextSlideId+1})`, { x: '100%', duration: .5 });
+    tl.from(`.slide:nth-child(${nextSlideId+1})`, { x: this.previous ? '-100%' : '100%', duration: .5 });
     tl.set(`.slide:nth-child(${this.activeSlideId+1})`, { display: 'none' });
     this.textAnimation(nextSlideId, tl);
+    this.previous = false
+  }
+  sliderIntervalReset() {
+    clearInterval(this.sliderInterval);
+    this.sliderInterval = setInterval(() => this.nextSlide(), 5000);
   }
   swipe() {
     let cords = {
@@ -42,7 +55,8 @@ class Slider {
       cords.start = {
         clientX,
         clientY
-      }
+      };
+      cords.end = cords.start;
     });
     slider.addEventListener('touchmove', e => {
       const { clientX, clientY } = e.touches[0];
@@ -52,9 +66,13 @@ class Slider {
       }
     })
     slider.addEventListener('touchend', e => {
-      clearInterval(this.sliderInterval);
-      cords.start.clientX > cords.end.clientX ? this.nextSlide() : this.nextSlide(this.activeSlideId - 1 === 0 ? this.slides.length : this.activeSlideId - 1);
-      this.sliderInterval = setInterval(() => this.nextSlide(), 5000);
+      const diffX = Math.abs(cords.start.clientX - cords.end.clientX);
+      const diffY = Math.abs(cords.start.clientY - cords.end.clientY)
+      if (diffX > 50 & diffY < diffX) {
+        this.previous = cords.start.clientX < cords.end.clientX ? true : false;
+        this.nextSlide();
+        this.sliderIntervalReset();
+      }
     });
   }
   textAnimation(nextSlideId, tl) {
